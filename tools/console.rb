8e47feb72d3ef7c1b67bb3309471a1c23079a000
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-10-14 13:47:30 -0700

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,41 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+sarah = Follower.new(23)
+
+
+illuminati = Cult.new("federal reserve")	
+illuminati.name="illuminati"
+illuminati.founding_year=1776
+illuminati.slogan="In God We Trust"
+scientology = Cult.new("california")	
+scientology.name = "scientology"
+scientology.founding_year = 1770
+scientology.slogan = "We love Tom Cruise"
+branchdavidians = Cult.new("texas")	
+branchdavidians.name = "branch davidians"
+branchdavidians.founding_year = 1993
+branchdavidians.slogan = "Come at us bro"
+
+ladygaga = Follower.new(25)	
+ladygaga.name = "Lady Gaga"
+ladygaga.life_motto = "Bad Romance"
+
+tomcruise = Follower.new(54)
+tomcruise.name = "Tom Cruise"
+tomcruise.life_motto =  "I love my ex-wife"
+
+david = Follower.new(35)	
+david.name = "David Branch"
+david.life_motto = "I'm Jesus"
+
+sarah = Follower.new(23)
+sarah.name = "Sarah"
+sarah.life_motto = "I love cats"
+
+oath1 = BloodOath.new(ladygaga, illuminati)	
+oath2 = BloodOath.new(tomcruise, scientology)	
+oath3 = BloodOath.new(david, branchdavidians)
 
 binding.pry
 
